65d4b32b20718d25af4385f0d82123aedc55a5ce
Manisha Jadhav <manishajadhav2323@gmail.com>
2022-05-11 14:12:50 +0530

    plugin hapi-geo-locate

--- server.js
+++ server.js
@@ -5,34 +5,66 @@ const server = new Hapi.Server({
   port: "3000",
 });
 
-server.route({
-  method: "GET",
-  path: "/",
-  handler: (request, h) => {
-    return "<h1> Welcome to the HAPI .....</h1>";
-  },
-});
-
-server.route({
-    method:'GET',
-    path : '/superhero/{heroName}',
-    handler: (request,h)=>
-    {
-        return `<h1>Incrediable ${request.params.heroName}</h1>`
-    }
-
-})
-
-server.start(async(err) => {
-  console.log("started..");
-  if (err) {
-    throw err;
-  }
-  await console.log(` Server Started on port`, server.info.uri);
-});
+const init  = async() =>
+{
+    await server.register({
+        plugin :require('hapi-geo-locate'),
+        options:
+        {
+            enabledByDefault : true
+        }
+    })
+    
+    server.route({
+      method: "GET",
+      path: "/",
+      handler: (request, h) => {
+        return "<h1> Welcome to the HAPI .....</h1>";
+      },
+    });
+    
+    server.route({
+        method:'GET',
+        path : '/superhero/{heroName}',
+        handler: (request,h)=>
+        {
+            return `<h1>Incrediable ${request.params.heroName}</h1>`
+        }
+    
+    })
+    
+    server.route({
+        method : 'GET',
+        path : '/{any*}',
+        handler : (request, reply) =>
+        {
+            return "<h1>Page not found !</h1>"
+        }
+    })
+    
+    server.route({
+        method: 'GET',
+        path : '/getLocation',
+        handler: (request,h)=>
+        {
+            return request.location
+        }
+    })
+    
+    await server.start(async(err) => {
+      console.log("started..");
+      if (err) {
+        throw err;
+      }
+      await console.log(` Server Started on port`, server.info.uri);
+    });
+    
+}
 
 process.on('uncaughtException',(err)=>
 {
     console.log(err)
     process.exit()
 })
+
+init()
