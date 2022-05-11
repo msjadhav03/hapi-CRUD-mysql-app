40d731d1cf98463d38568ac3b0747cae82f60865
Manisha Jadhav <manishajadhav2323@gmail.com>
2022-05-11 11:41:37 +0530

    Inital Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const Hapi = require("hapi");
+
+const server = new Hapi.Server({
+  host: "localhost",
+  port: "3000",
+});
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (request, h) => {
+    return "<h1> Welcome to the HAPI .....</h1>";
+  },
+});
+
+server.start(async(err) => {
+  console.log("started..");
+  if (err) {
+    throw err;
+  }
+  await console.log(` Server Started on port`, server.info.uri);
+});
+
+process.on('uncaughtException',(err)=>
+{
+    console.log(err)
+    process.exit()
+})
