0d35d9f434577f6011fd4e279451d80529f346a0
Manisha Jadhav <manishajadhav2323@gmail.com>
2022-05-11 15:23:10 +0530

    serving static files

--- server.js
+++ server.js
@@ -1,70 +1,78 @@
-const Hapi = require("hapi");
+const Hapi = require("@hapi/hapi");
+const inert = require("@hapi/inert");
+
+const path = require('path')
 
 const server = new Hapi.Server({
   host: "localhost",
   port: "3000",
+  routes:
+  {
+      files:
+      {
+          relativeTo : path.join(__dirname, 'static'),
+      }
+  }
 });
 
-const init  = async() =>
-{
-    await server.register({
-        plugin :require('hapi-geo-locate'),
-        options:
-        {
-            enabledByDefault : true
-        }
-    })
-    
-    server.route({
-      method: "GET",
-      path: "/",
-      handler: (request, h) => {
-        return "<h1> Welcome to the HAPI .....</h1>";
+const init = async () => {
+  await server.register([
+    {
+      plugin: require("hapi-geo-locate"),
+      options: {
+        enabledByDefault: true,
       },
-    });
-    
-    server.route({
-        method:'GET',
-        path : '/superhero/{heroName}',
-        handler: (request,h)=>
-        {
-            return `<h1>Incrediable ${request.params.heroName}</h1>`
-        }
-    
-    })
-    
-    server.route({
-        method : 'GET',
-        path : '/{any*}',
-        handler : (request, reply) =>
-        {
-            return "<h1>Page not found !</h1>"
-        }
-    })
-    
-    server.route({
-        method: 'GET',
-        path : '/getLocation',
-        handler: (request,h)=>
-        {
-            return request.location
-        }
-    })
-    
-    await server.start(async(err) => {
-      console.log("started..");
-      if (err) {
-        throw err;
-      }
-      await console.log(` Server Started on port`, server.info.uri);
-    });
-    
-}
+    },
+    {
+      plugin: inert,
+    },
+  ]);
 
-process.on('uncaughtException',(err)=>
-{
-    console.log(err)
-    process.exit()
-})
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return h.file('html/index.html')
+    }
+   
+  });
 
-init()
+  server.route({
+    method: "GET",
+    path: "/superhero/{heroName}",
+    handler: (request, h) => {
+      return `<h1>Incrediable ${request.params.heroName}</h1>`;
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/{any*}",
+    handler: (request, reply) => {
+      return "<h1>Page not found !</h1>";
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/getLocation",
+    handler: (request, h) => {
+      return request.location;
+    },
+  });
+
+  await server.start(async (err) => {
+    console.log("started..");
+    if (err) {
+      throw err;
+    }
+    await console.log(` Server Started on port`, server.info.uri);
+  });
+};
+
+process.on("uncaughtException", (err) => {
+  console.log(err);
+  process.exit();
+});
+
+init();
